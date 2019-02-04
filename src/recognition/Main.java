e8f7ea169c0c25f837682466c41696961a3bb2e8
Kirill <thegaldren@gmail.com>
2019-02-04 07:11:00 +0300

    Add default weights and bias

--- src/recognition/Main.java
+++ src/recognition/Main.java
@@ -1,6 +1,9 @@
 package recognition;
 
 public class Main {
+    static final int[] w = {2, 1, 2, 4, -4, 4, 2, -1, 2};
+    static final int   b = -5;
+
     public static void main(String[] args) {
         System.out.print("Hello world!");
     }
