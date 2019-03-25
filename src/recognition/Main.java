a90719dc04cc415ebfa0009537d1a9f7a3fcf996
Kirill <thegaldren@gmail.com>
2019-03-25 11:58:02 +0300

    Скелет интерфейса

--- src/recognition/Main.java
+++ src/recognition/Main.java
@@ -1,7 +1,37 @@
 package recognition;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("\nWhat do you want?");
+            System.out.println("1. Teach the network");
+            System.out.println("2. Guess a number");
+            System.out.println("3. Exit");
+            System.out.print("Your choice: ");
+
+            switch(scanner.nextInt()){
+                case 1:{
+                    // TODO
+                    System.out.println("\nTODO");
+                    break;
+                }
+                case 2:{
+                    // TODO
+                    System.out.println("\nTODO");
+                    break;
+                }
+                case 3:{
+                    System.out.println("\nBye bye!");
+                    System.exit(0);
+                }
+                default:
+                    System.out.println("\nWrong input");
+            }
+        }
     }
 }
