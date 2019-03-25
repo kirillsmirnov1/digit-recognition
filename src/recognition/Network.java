1d6e0b497671a523b10b879e8e9b5eb9af99edf1
Kirill <thegaldren@gmail.com>
2019-03-25 12:19:00 +0300

    Класс сети

---
+++ src/recognition/Network.java
@@ -0,0 +1,51 @@
+package recognition;
+
+import java.util.Random;
+
+public class Network {
+
+    // Количество слоёв
+    private final int numberOfLayers;
+
+    // Количество нейронов на каждом слое
+    private final int[] layerSizes;
+
+    // Слои нейронов
+    private double[][] layers;
+
+    // Веса между слоями
+    private double[][][] weights;
+
+    public Network(int numberOfLayers){
+        this(new int[] {15, 10});
+    }
+
+    public Network(int[] layerSizes){
+        Random random = new Random();
+
+        this.layerSizes = layerSizes;
+        numberOfLayers = layerSizes.length;
+
+        // Инициализирую пустой массив слоёв
+        layers = new double[numberOfLayers][];
+
+        // Инициализирую каждый слой
+        for(int layer = 0; layer < numberOfLayers; ++layer){
+            layers[layer] = new double[layerSizes[layer]];
+        }
+
+        // Инициализирую массив весов
+        weights = new double[numberOfLayers - 1][][];
+
+        // Заполняю веса случайными значениями
+        for(int midLayer = 0; midLayer < numberOfLayers - 1; ++midLayer){
+            weights[midLayer] = new double[layerSizes[midLayer]][layerSizes[midLayer+1]];
+
+            for(int i = 0; i < layerSizes[midLayer]; ++i){
+                for(int j = 0; j < layerSizes[midLayer+1]; ++j){
+                    weights[midLayer][i][j] = random.nextGaussian();
+                }
+            }
+        }
+    }
+}
