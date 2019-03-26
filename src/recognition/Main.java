6a973d334dbaa461b0d35db1fa71ca330f61116b
Kirill <thegaldren@gmail.com>
2019-03-26 06:31:38 +0300

    Метод чтения числа из консоли

--- src/recognition/Main.java
+++ src/recognition/Main.java
@@ -1,5 +1,6 @@
 package recognition;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -33,5 +34,22 @@ public class Main {
                     System.out.println("\nWrong input");
             }
         }
+    }
+
+    // ????? ?? ??????? ????? ? ???? ??????? 5?3
+    private static double[] readInput() {
+        double[] a = new double[15];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input 5x3 grid ('_' for white, any other for blue):");
+        for(int i=0; i < 5; ++i){
+            String str = scanner.nextLine();
+            for(int j = 0; j < 3; ++j){
+                a[3*i + j] =  (str.charAt(j) == '_') ? 0 : 1;
+            }
+        }
+
+        System.out.println("\nInput:\n" + Arrays.toString(a) + "\n");
+
+        return a;
     }
 }
